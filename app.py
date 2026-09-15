7df6d06828a3e33030aa2a6de8e1da964f51552f
Tarunsinghverma <tarunverma7215@gmail.com>
2026-09-15 12:28:55 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+#step 1: Load Important modules
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score
+import streamlit as st
+# this streamlit is for web based application projects
